d87b9cae2c52ba8f0d38f99c2c89bc04135baad7
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-19 09:04:54 +0900

    Move Dactylo to Atelier

--- src/assets/js/copy-buttons.js
+++
@@ -1,6 +0,0 @@
-import { addCopyButtons } from '@remino/functions'
-
-addCopyButtons({
-	blockSelector: 'main .code-block',
-	copiedLabel: 'Copied',
-})
--- src/data/site.ts
+++
@@ -1,9 +0,0 @@
-export const site = {
-	description: 'Typewriter effect in JS using CSS.',
-	image: '/dactylo/share.avif.jpg',
-	imageAlt: 'Dactylo share image',
-	readableTitle: 'Dactylo',
-	title: 'dactylo',
-	twitterHandle: '@remino',
-	url: 'https://remino.net/dactylo/',
-}
